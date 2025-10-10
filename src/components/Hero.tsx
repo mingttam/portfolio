67c10b8fa5b7808794fa6a2437440630c792ca19
mingttam <mingtam.713@gmail.com>
2025-10-11 03:28:18 +0700

    refactor: grid of Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,8 +18,8 @@ const Hero = () => {
       <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100  ">
         <div
           className={cn(
-            "absolute inset-0",
-            "[background-size:40px_40px]",
+            "absolute inset-0 ",
+            "[background-size:60px_60px]",
             "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
             "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
           )}
