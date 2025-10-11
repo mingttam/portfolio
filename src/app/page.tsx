182332d977ebdfa9fb05ddc2d536b6c32802ce7e
mingttam <mingtam.713@gmail.com>
2025-10-11 17:59:03 +0700

    feat: Add RecentProjects component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import { Contact, FolderDot, House, Mail } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          className=""
-          navItems={[
-            { name: "Home", link: "/", icon: <House /> },
-            { name: "About", link: "/about", icon: <Contact /> },
-            { name: "Projects", link: "/projects", icon: <FolderDot /> },
-            { name: "Contact", link: "/contact", icon: <Mail /> },
-          ]}
-        />
+        <FloatingNav className="" navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
