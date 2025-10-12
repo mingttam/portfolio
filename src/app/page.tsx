6b6b5d764917f6c3c169ad3e7ce0adf93bce6c22
mingttam <mingtam.713@gmail.com>
2025-10-12 22:21:32 +0700

    feat: Add Footer component with social media links and contact button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -8,11 +9,12 @@ import { Contact, FolderDot, House, Mail } from "lucide-react";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+      <div className="max-w-6xl w-full">
         <FloatingNav className="" navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
+        <Footer />
       </div>
     </main>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image";
+import MagicButton from "./ui/MagicButton";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pb-10 relative" id="contact">
+      <div>
+        <Image
+          src="/footer-grid.svg"
+          alt="Footer Image"
+          width={800}
+          height={500}
+          style={{ objectFit: "cover" }}
+          className="opacity-50"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center text-center mt-10 space-y-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="heading lg:max-w-[45vw]">Ready to build something great together? </h1>
+        <a
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=mingtam.713@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <MagicButton title="Contact Me" />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-grow flex-col justify-between items-center">
+        <p className="text-white-200 md:mt-base text-sm md:font-normal font-light my-5 text-center">
+          © 2025 Minh Tam. All rights reserved.
+        </p>
+        <div className=" flex items-center md:gap-3 gap-6">
+          {socialMedia.map(({ id, img, link }) => (
+            <a
+              key={id}
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white-200 hover:text-white-100 transition-colors cursor-pointer"
+            >
+              <Image src={img} alt="socialIcon" width={24} height={24} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
